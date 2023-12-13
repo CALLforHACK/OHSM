a80cea52ea9cfe7657ef39ae4e52d3481ff990f1
VISWAJEET BHARDWAJ <88625027+CALLforHACK@users.noreply.github.com>
2023-12-13 13:43:59 +0530

    Add files via upload

---
+++ OHSM-BE/README.md
@@ -0,0 +1,4 @@
+# OHSM BE
+
+--npm install (to install dependency)
+--npm start (to start the server)
---
+++ OHSM-BE/models/InventorySchema.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const InventorySchema = new mongoose.Schema({
+  propertyname: {
+    type: String,
+    required: true,
+  },
+  propertyInventoryType: {
+    type: String,
+    required: true,
+  },
+  otherPropertyInventoryType: {
+    type: String,
+  },
+  capacity: {
+    type: Number,
+    required: true,
+  },
+  amenities: {
+    type: String,
+  },
+  availabilityStatus: {
+    type: String,
+    required: true,
+  },
+  notes: {
+    type: String,
+  },
+});
+
+const Inventory = mongoose.model('Inventory', InventorySchema); // Change the model name to 'Inventory'
+
+module.exports = Inventory;
